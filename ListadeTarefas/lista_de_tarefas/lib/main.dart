8e5ea3024f32ab9b4b9334475bd6d16fb198915c
GustavoMendes1 <54938008+GustavoMendes1@users.noreply.github.com>
2020-04-21 14:22:41 -0300

    Update main.dart

--- ListadeTarefas/lista_de_tarefas/lib/main.dart
+++ ListadeTarefas/lista_de_tarefas/lib/main.dart
@@ -130,6 +130,7 @@ class _HomeState extends State<Home> {
                 });
               }),
           );
+          Scaffold.of(context).removeCurrentSnackBar();    
           Scaffold.of(context).showSnackBar(snack);
         });
       },
